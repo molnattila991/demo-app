4a6023f170f3b8b7ce32f56f23d79f2135e5d8df
molnattila991 <78646066+molnattila991@users.noreply.github.com>
2021-07-30 08:57:50 +0200

    Update feature-a-page-container.component.ts

--- projects/features/src/lib/feature-a/components/feature-a-page-container/feature-a-page-container.component.ts
+++ projects/features/src/lib/feature-a/components/feature-a-page-container/feature-a-page-container.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-feature-a-page-container',
   templateUrl: './feature-a-page-container.component.html',
-  styleUrls: ['./feature-a-page-container.component.css']
+  styleUrls: ['./feature-a-page-container.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FeatureAPageContainerComponent implements OnInit {
 
